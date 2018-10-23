30d052102d9cfbf4dc709a7345a0f173c8ca4b9c
wangxingsheng <164587694@qq.com>
2018-10-23 18:00:19 +0800

    实现顺序式程序cache

--- mrthumb/src/main/java/com/reone/mrthumb/process/OrderCacheProcess.java
+++ mrthumb/src/main/java/com/reone/mrthumb/process/OrderCacheProcess.java
@@ -2,13 +2,14 @@ package com.reone.mrthumb.process;
 
 import android.graphics.Bitmap;
 
+import com.reone.mrthumb.cache.ThumbCache;
 import com.reone.mrthumb.listener.ThumbProvider;
 
 /**
  * Created by wangxingsheng on 2018/10/17.
  * desc:顺序填充
  */
-public class OrderCacheProcess extends CacheProcess{
+public class OrderCacheProcess extends CacheProcess {
 
     public OrderCacheProcess(ThumbProvider thumbProvider) {
         super(thumbProvider);
@@ -16,16 +17,23 @@ public class OrderCacheProcess extends CacheProcess{
 
     @Override
     void start() {
-
+        for (int i = 0; i < maxSize; i++) {
+            if (ThumbCache.getInstance().hasThumbnail(i)) return;
+            try {
+                ThumbCache.getInstance().set(i, getThumbProvider().getIndex(i));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     Bitmap get(int index) {
-        return null;
+        return ThumbCache.getInstance().get(index);
     }
 
     @Override
     void release() {
-
+        ThumbCache.getInstance().release();
     }
 }
